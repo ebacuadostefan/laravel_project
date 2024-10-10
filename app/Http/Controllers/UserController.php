218fcfe9592be7f079d2ba1bef8bd5a9abfcf435
ebacuadostefan <ebacuadomentino@gmail.com>
2024-10-10 12:08:28 +0800

    2nd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\users;
+use Illuminate\validation\Rules;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -27,7 +31,26 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validated([
+            'name' => ['required','string','max:255'],
+            'email' => ['required','string','lowercase','email','max:255','unique',User::class],
+            'password' => ['required','confirmed',Rules\Password::defaults()],
+            'profile' => ['nullable','image','mimes:jpg,jpeg,png','max:2048'],
+        ]);
+
+        if (request->hasFile('profile')) {
+            $filenameWithExtension = $request->file('profile')->getClientOriginalName();
+            $filename = pathinfo($filenameWithExtension, PATHINFO_FILENAME);
+            $extension = $request->file('profile')->getClientOriginalExtension();
+            $filenameToStore = $filename . '_' . time() . '.' . $extension;
+            $request->file('profile')->storeAs('Uploads/users-profile',$filenameToStore);
+            $validated['profile'] = $filenameToStore;
+            
+        }
+
+        $user = User::create(validated);
+
+        return redirect(route('users'));
     }
 
     /**
